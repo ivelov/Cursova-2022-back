83f8652b403a21f1fef02f7286d9d361fe0066e5
ivelov <ivelov.vlad1@gmail.com>
2022-11-28 16:39:34 +0200

    feat: add time validation to reports

--- app/Nova/Report.php
+++ app/Nova/Report.php
@@ -3,6 +3,8 @@
 namespace App\Nova;
 
 use App\Models\Conferences;
+use App\Models\Report as ModelsReport;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Laravel\Nova\Fields\BelongsTo;
@@ -66,11 +68,11 @@ class Report extends Resource
 
             TimeField::make('Start time')
                 ->sortable()
-                ->rules('required', 'after:now'),
+                ->rules('required'),
                
             TimeField::make('End time')
                 ->sortable()
-                ->rules('required', 'after:start_time'),
+                ->rules('required', 'after:start_time', 'before:21:00'),
             
             Textarea::make('Description')->nullable(),
 
@@ -78,7 +80,8 @@ class Report extends Resource
                 ->acceptedTypes('.pptx,.ppt')
                 ->disk('public')
                 ->path('presentations')
-                ->nullable(),
+                ->nullable()
+                ->rules('max:10000'),
 
             BelongsTo::make('User')
                 ->rules('required'),
@@ -102,7 +105,7 @@ class Report extends Resource
     protected static function afterValidation(NovaRequest $request, $validator)
     {
         $conference = Conferences::findOrFail($request->conference);
-        $conferenceStartTime = strtotime($conference->time);
+        $conferenceStartTime = strtotime($conference->time) + env('TIMEZONE_DIFF_SECONDS', 7200);
         $startTime = strtotime($request->start_time);
         $endTime = strtotime($request->end_time);
         
@@ -123,6 +126,98 @@ class Report extends Resource
                 'Report maximal duration is 60 min.'
             );
         }
+
+        if($request->editMode == 'create'){
+            $reports = ModelsReport::where('conference_id', $request->conference)->get();
+        }else{
+            $reports = ModelsReport::where('conference_id', $request->conference)
+            ->where('title', '<>', $request->title)
+            ->get();
+        }
+        
+        $startTimes = collect();
+        $endTimes = collect();
+        
+        foreach ($reports as $report) {
+            $startTimes->push(strtotime($report->start_time));
+            $endTimes->push(strtotime($report->end_time));
+        }
+        $startTimes = $startTimes->sort();
+        $endTimes = $endTimes->sort();
+        
+
+
+        $timesCount = $startTimes->count();
+        $nearestConflict = false;
+        //Check if report time conflicts with
+        for ($i = 0; $i < $timesCount; $i++) {
+            if($startTime > $startTimes[$i]){
+                if($startTime < $endTimes[$i]){
+                    $nearestConflict = $i;
+                    break;
+                }
+            }else if($startTimes[$i] < $endTime){ //if another report in a report interval
+                $nearestConflict = $i;
+                break;
+            }
+            if($endTime > $startTimes[$i]){
+                if($endTime < $endTimes[$i]){
+                    $nearestConflict = $i;
+                    break;
+                }
+            }
+        }
+
+        if($nearestConflict){
+
+            $nearestAvailableStartTime = false;
+            $nearestAvailableEndTime = false;
+            for ($i = 0; $i < $timesCount; $i++) { 
+                if($i == 0){
+                    if($startTimes[$i] - $conferenceStartTime > 299){
+                        $nearestAvailableStartTime = $conferenceStartTime;
+                        $nearestAvailableEndTime = $startTimes[$i];
+                    }
+                }else if($i + 1 == $timesCount){
+                    if(strtotime('21:00') - $endTimes[$i] > 299){
+                        if(!$nearestAvailableStartTime){
+                            $nearestAvailableStartTime = $endTimes[$i];
+                            $nearestAvailableEndTime = strtotime('21:00');
+                        }else if(abs($endTimes[$i] - $startTime) < abs($nearestAvailableStartTime - $startTime)){
+                            $nearestAvailableStartTime = $endTimes[$i];
+                            $nearestAvailableEndTime = strtotime('21:00');
+                        }
+                    }
+                    break;
+                }
+                //if there is a long pause between reports
+                if($startTimes[$i+1] - $endTimes[$i] > 299){
+                    if(!$nearestAvailableStartTime){
+                        $nearestAvailableStartTime = $endTimes[$i];
+                        $nearestAvailableEndTime = $startTimes[$i+1];
+                    }else if(abs($endTimes[$i] - $startTime) < abs($nearestAvailableStartTime - $startTime)){
+                        $nearestAvailableStartTime = $endTimes[$i];
+                        $nearestAvailableEndTime = $startTimes[$i+1];
+                    }
+                }
+                
+            }
+
+            if(!$nearestAvailableStartTime){
+                $validator->errors()->add(
+                    'start_time',
+                    'Sorry, the conference is busy all the time.'
+                );
+            }else{
+                $validator->errors()->add(
+                    'start_time',
+                    'Report time conflicts with other reports. Nearest interval: '.date('H:i',$nearestAvailableStartTime).'-'.date('H:i',$nearestAvailableEndTime).'.'
+                );
+            }
+            
+
+        }
+        
     }
 
     /**
